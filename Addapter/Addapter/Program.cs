031552c4a5eed766fbe9368113b70c1ff1e63579
unknown <ozabello21@gmail.com>
2022-12-13 13:53:21 +0300

    an application has ben created in whish i show how to use adapter pattern

---
+++ Addapter/Addapter/Program.cs
@@ -0,0 +1,57 @@
+﻿using System.Threading.Channels;
+//Main
+Car car = new Car();
+HorseToTransportAdapter horse = new HorseToTransportAdapter();
+
+
+Human human = new Human();
+human.Travel(car);
+human.Travel(horse);
+
+
+//transport
+public interface Itransport
+{
+    public void Drive();
+}
+
+
+public class Car :Itransport
+{
+    public void Drive()
+    {
+        Console.WriteLine("the car is comming");
+    }
+}
+//adapte
+public interface IAnimal
+{
+    public void Move();
+}
+
+public class Horse:IAnimal
+{
+    public void Move()
+    {
+        Console.WriteLine("the horse is comming");
+    }
+}
+//adapter
+public class HorseToTransportAdapter:Itransport
+{
+    Horse horse =new Horse();
+    public void Drive()
+    {
+        horse.Move();
+    }
+}
+
+//additional classes
+
+public class Human
+{
+    public void Travel(Itransport transport)
+    {
+        transport.Drive();
+    }
+}
